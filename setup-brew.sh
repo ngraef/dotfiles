cf9e1640f34bb3945abb24624fa77d3367ad6047
Nick Graef <1031317+ngraef@users.noreply.github.com>
2022-05-31 19:13:20 -0500

    configure brew shell env

--- setup-brew.sh
+++ setup-brew.sh
@@ -4,5 +4,9 @@ set -e
 echo "Installing Homebrew"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+echo "Configuring Homebrew shell env"
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/$(whoami)/.zshenv
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
 echo "Installing packages from brew bundle"
-brew bundle
+brew bundle install
